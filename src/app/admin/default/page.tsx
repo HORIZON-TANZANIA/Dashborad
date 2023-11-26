b923723990a2b8a68455de27bfe3089380a85df9
Daniel Mawalla <mawalladaniel2018@gmail.com>
2023-11-26 13:17:05 +0300

    changes

--- src/app/admin/default/page.tsx
+++ src/app/admin/default/page.tsx
@@ -23,34 +23,20 @@ const Dashboard = () => {
       <div className="mt-3 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-3 3xl:grid-cols-6">
         <Widget
           icon={<MdBarChart className="h-7 w-7" />}
-          title={'Earnings'}
-          subtitle={'$340.5'}
+          title={'Loyal Customers'}
+          subtitle={'5,896'}
         />
         <Widget
           icon={<IoDocuments className="h-6 w-6" />}
-          title={'Spend this month'}
-          subtitle={'$642.39'}
+          title={'Generated Revenue'}
+          subtitle={'32,000,000 TSh'}
         />
         <Widget
           icon={<MdBarChart className="h-7 w-7" />}
-          title={'Sales'}
-          subtitle={'$574.34'}
+          title={'Redeemed Rewards'}
+          subtitle={'89,878'}
         />
-        <Widget
-          icon={<MdDashboard className="h-6 w-6" />}
-          title={'Your Balance'}
-          subtitle={'$1,000'}
-        />
-        <Widget
-          icon={<MdBarChart className="h-7 w-7" />}
-          title={'New Tasks'}
-          subtitle={'145'}
-        />
-        <Widget
-          icon={<IoMdHome className="h-6 w-6" />}
-          title={'Total Projects'}
-          subtitle={'$2433'}
-        />
+    
       </div>
 
       {/* Charts */}

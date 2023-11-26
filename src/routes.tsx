fac35198104ada1de719b9f12c9b0bceb7332045
Daniel Mawalla <mawalladaniel2018@gmail.com>
2023-11-26 13:10:06 +0300

    changes

--- src/routes.tsx
+++ src/routes.tsx
@@ -13,13 +13,13 @@ import {
 
 const routes = [
   {
-    name: 'Main Dashboard',
+    name: 'Dashboard',
     layout: '/admin',
     path: 'default',
     icon: <MdHome className="h-6 w-6" />,
   },
   {
-    name: 'NFT Marketplace',
+    name: 'Customers',
     layout: '/admin',
     path: 'nft-marketplace',
     icon: <MdOutlineShoppingCart className="h-6 w-6" />,
@@ -27,28 +27,23 @@ const routes = [
     secondary: true,
   },
   {
-    name: 'Data Tables',
+    name: 'Rewards',
     layout: '/admin',
     icon: <MdBarChart className="h-6 w-6" />,
     path: 'data-tables',
   },
   {
-    name: 'Profile',
+    name: 'Settings',
     layout: '/admin',
     path: 'profile',
     icon: <MdPerson className="h-6 w-6" />,
   },
   {
-    name: 'Sign In',
+    name: 'Reviews',
     layout: '/auth',
     path: 'sign-in',
     icon: <MdLock className="h-6 w-6" />,
   },
-  {
-    name: 'RTL Admin',
-    layout: '/rtl',
-    path: 'rtl-default',
-    icon: <MdHome className="h-6 w-6" />,
-  },
+  
 ];
 export default routes;
